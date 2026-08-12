9c33308b7de6365d2af7859a83f0a68496f7fa76
davirbicalho01-hue <davirbicalho01@gmail.com>
2026-08-12 17:04:06 -0300

    catalogo

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,3 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import Automovel.Peca;
+import Automovel.Veiculo;
+
 public class Catalogo {
-    
+    private List<Peca> pecas;
+    private List<Compatibilidade> compatibilidades;
+
+    public Peca buscarPecaPorNome(String nome) {
+    for (Peca peca : pecas) {
+        if (peca.getNome().equalsIgnoreCase(nome)) {
+            return peca;
+        }
+    }
+
+    return null;
+}
+
+public List<Peca> buscarPecasPorVeiculo(Veiculo veiculo) {
+    List<Peca> resultado = new ArrayList<>();
+
+    for (Compatibilidade compatibilidade : compatibilidades) {
+        if (compatibilidade.getVeiculo().equals(veiculo)) {
+            resultado.add(compatibilidade.getPeca());
+        }
+    }
+
+    return resultado;
+}
 }
